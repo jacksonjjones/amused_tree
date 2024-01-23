151d7cb955fc1bd643f654a4b2bb87df4db59335
Jackson Jones <jacksonjones@Jacksons-MacBook-Pro.local>
2024-01-23 15:26:01 -0600

    updated html, css, js with screenshot of app

---
+++ Develop/script.js
@@ -0,0 +1,47 @@
+  var currentDayEl = $('#currentDay');
+
+  $('.saveBtn').click(function(){
+
+    var timeBlockId = $(this).parent().attr('id');
+    var userInput = $(this).siblings('.description').val();
+    localStorage.setItem(timeBlockId, userInput);
+    
+      $('.add-message').removeClass("hidden");
+          setTimeout(function(){
+          $('.add-message').addClass("hidden");
+      },2000);
+  });
+  
+  $('.time-block').each(function() {
+      var currentHour = dayjs().hour();
+      var blockHour = parseInt($(this).attr('id').split("-")[1]);
+
+      if (blockHour < currentHour) {
+          $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+          $(this).addClass('present');
+      } else {
+          $(this).addClass('future');
+      }
+  });
+
+  $(".description").each(function() {
+    var timeBlockId = $(this).parent().attr("id");
+    var savedData = localStorage.getItem(timeBlockId);
+    if (savedData) {
+      $(this).val(savedData);
+    }
+  });
+
+  function displayCurrentDay() {
+    var currentDay = dayjs().format('dddd, MMMM D[th]');
+    currentDayEl.text(currentDay);
+  }
+
+displayCurrentDay();
+
+
+
+
+
+
